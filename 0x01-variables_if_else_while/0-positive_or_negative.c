5c776d4200c74ffc8a7c183b30621f32bb2f8449
Walexity20 <walexity20@gmail.com>
2022-10-14 18:59:07 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d is negative\n", n)
+printf("%d is negative\n", n);
 }
 else if (n == 0)
 {
@@ -20,7 +20,7 @@ printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is positive\n", n);
+printf("%d is positive\n", n)
 }
 return (0);
 }
